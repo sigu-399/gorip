4e3401d3289c42383f1900bf26ad0f1ddec16c60
sigu-399 <sigu.399@gmail.com>
2013-03-04 19:31:38 +0800

    formatting

--- src/gorip/server.go
+++ src/gorip/server.go
@@ -1,13 +1,13 @@
-// author           sigu-399
-// author-github    https://github.com/sigu-399
-// author-mail      sigu.399@gmail.com
+// author			sigu-399
+// author-github	https://github.com/sigu-399
+// author-mail		sigu.399@gmail.com
 // 
-// repository-name  gorip
-// repository-desc  REST Server Framework - ( gorip: REST In Peace ) - Go language
+// repository-name	gorip
+// repository-desc	REST Server Framework - ( gorip: REST In Peace ) - Go language
 // 
-// description      Server implementation.
+// description		Server implementation.
 // 
-// created          03-03-2013
+// created			03-03-2013
 
 package gorip
 
